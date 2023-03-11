57be279dfa108be37e1bafa89bc42c5ff3a70a18
Mark Pentler <tehhustler@hotmail.com>
2023-03-11 21:17:43 +0000

    we have DNS!

--- update.sh
+++ update.sh
@@ -232,7 +232,7 @@ systemctl is-active oarc-adsb-feed &>/dev/null || {
 echo 96
 
 if [[ -f /etc/default/oarc-adsb ]]; then
-    sed -i -e 's/44.31.91.230,30004,beast_reduce_out/44.31.91.230,30004,beast_reduce_out/' /etc/default/oarc-adsb || true
+    sed -i -e 's/adsb.oarc.uk,30004,beast_reduce_out/adsb.oarc.uk,30004,beast_reduce_out/' /etc/default/oarc-adsb || true
 fi
 
 
