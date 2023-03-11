5338456cfe1adfadec55fe37cbf0ef8a04f1cb1a
Mark Pentler <tehhustler@hotmail.com>
2023-03-11 18:04:59 +0000

    bash script bug stopping install fixed

--- configure.sh
+++ configure.sh
@@ -137,7 +137,8 @@ RESULTS4="--results beast,connect,127.0.0.1:30169"
 PRIVACY=""
 INPUT_TYPE="$INPUT_TYPE"
 
-# MLATSERVER="44.31.91.230:31090" - not yet!
+# mlatserver is unused at this time
+MLATSERVER="44.31.91.230:31090"
 TARGET="--net-connector 44.31.91.230,30004,beast_reduce_out"
 NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30169 --net-bo-port 0 --net-ri-port 0 --write-json-every 1 --uuid-file /usr/local/share/oarc-adsb/oarc-adsb-uuid"
 JSON_OPTIONS="--max-range 450 --json-location-accuracy 2 --range-outline-hours 24"
